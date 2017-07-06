1c5d02edfb634f1579e65e6c4d526077bce36f65
David Jung <david.jung@taggle.co>
2017-07-06 09:51:03 +0900

    Language / Locale 을 출력하는 기능 추가

--- GetLanguageExample/GetLanguageExample/ViewController.swift
+++ GetLanguageExample/GetLanguageExample/ViewController.swift
@@ -12,14 +12,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
+        preferLabel.text = "Language: \(Locale.preferredLanguages[0]) Locale: \((Locale.current as NSLocale).object(forKey: .countryCode)!)"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBOutlet weak var preferLabel: UILabel!
 }
-
