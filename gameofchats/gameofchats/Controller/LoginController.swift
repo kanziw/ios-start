30666eb5a008e1af7128274d30e921df1be80348
David Jung <david.jung@peeper.co>
2017-12-29 14:54:05 +0900

    [gameofchats] 가운데 만들어지는 흰 뷰를 생성하고 셋업하는 과정을 함수로 추출

--- gameofchats/gameofchats/Controller/LoginController.swift
+++ gameofchats/gameofchats/Controller/LoginController.swift
@@ -10,25 +10,31 @@ import UIKit
 
 class LoginController: UIViewController {
     
+    let inputsContainerView :UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.white
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor(r: 61, g: 91, b: 151)
-        
-        let inputsContainerView = UIView()
-        inputsContainerView.backgroundColor = UIColor.white
-        inputsContainerView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(inputsContainerView)
-        
+        setupInputsContainerView()
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    func setupInputsContainerView() {
         // need x, y, width, height constraints
         inputsContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         inputsContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         inputsContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
         inputsContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
-    }
-    
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .lightContent
     }
 }
 
