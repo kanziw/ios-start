509c542a0a9e5325022d7e186fe9698de8c11db5
David Jung <david.jung@peeper.co>
2017-12-29 14:53:34 +0900

    [gameofchats] 중간에 위치하는 View 생성 및 constraint 추가

--- gameofchats/gameofchats/Controller/LoginController.swift
+++ gameofchats/gameofchats/Controller/LoginController.swift
@@ -14,6 +14,17 @@ class LoginController: UIViewController {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor(r: 61, g: 91, b: 151)
+        
+        let inputsContainerView = UIView()
+        inputsContainerView.backgroundColor = UIColor.white
+        inputsContainerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(inputsContainerView)
+        
+        // need x, y, width, height constraints
+        inputsContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        inputsContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        inputsContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+        inputsContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
