cfa76a59e93158777b3c2bc4391d2a4b4a67977d
kanziw <kanziwoong@gmail.com>
2017-12-29 23:52:09 +0900

    [gameofchats] constraint 를 이용해 기본이미지, textLabel, detailTextLabel 적용

--- gameofchats/gameofchats/Controller/NewMessageController.swift
+++ gameofchats/gameofchats/Controller/NewMessageController.swift
@@ -55,7 +55,9 @@ class NewMessageController: UITableViewController {
         cell.textLabel?.text = user.name
         cell.detailTextLabel?.text = user.email
         
-        cell.imageView?.image = #imageLiteral(resourceName: "gameofthrones_splash")
+        //        cell.imageView?.image = #imageLiteral(resourceName: "gameofthrones_splash")
+        //        cell.imageView?.contentMode = .scaleAspectFill
+        
         if let profileImageUrl = user.profileImageUrl, let url = URL(string: profileImageUrl) {
             URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
                 // download hit an error so lets return out
@@ -65,7 +67,7 @@ class NewMessageController: UITableViewController {
                 }
                 
                 DispatchQueue.main.async {
-                    cell.imageView?.image = UIImage(data: data!)
+                    //                    cell.imageView?.image = UIImage(data: data!)
                 }
             }).resume()
         }
@@ -75,8 +77,31 @@ class NewMessageController: UITableViewController {
 }
 
 class UserCell: UITableViewCell {
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        textLabel?.frame = CGRect(x: 56, y: textLabel!.frame.origin.y, width: textLabel!.frame.width, height: textLabel!.frame.height)
+        detailTextLabel?.frame = CGRect(x: 56, y: detailTextLabel!.frame.origin.y, width: detailTextLabel!.frame.width, height: detailTextLabel!.frame.height)
+    }
+    
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "gameofthrones_splash")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        self.addSubview(profileImageView)
+        
+        // ios 9 constraint anchors
+        // need x,y,width,height anchors
+        profileImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 8).isActive = true
+        profileImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        profileImageView.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        profileImageView.heightAnchor.constraint(equalToConstant: 40).isActive = true
     }
     
     required init?(coder aDecoder: NSCoder) {
