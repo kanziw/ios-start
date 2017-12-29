e1a0fdbb0be290b06ccc53aab33a236f16879ead
David Jung <david.jung@peeper.co>
2017-12-29 18:09:50 +0900

    [gameofchats] 로그인된 유저가 없으면 자동으로 로그인 화면으로 이동하는 코드 추가

--- gameofchats/gameofchats/Controller/ViewController.swift
+++ gameofchats/gameofchats/Controller/ViewController.swift
@@ -15,9 +15,20 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
+        
+        // user is not logged in
+        if Auth.auth().currentUser?.uid == nil {
+            perform(#selector(handleLogout), with: nil, afterDelay: 0)
+        }
     }
     
     @objc func handleLogout() {
+        do {
+            try Auth.auth().signOut()
+        } catch let logoutError {
+            print("", logoutError)
+        }
+        
         let loginController = LoginController()
         present(loginController, animated: true, completion: nil)
     }
