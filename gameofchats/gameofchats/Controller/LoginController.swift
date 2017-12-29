41885e1c1390e33a5789d01d3bb70af0ff23f97c
David Jung <david.jung@peeper.co>
2017-12-29 15:20:18 +0900

    [gameofchats] LoginRegisterButton 추가

--- gameofchats/gameofchats/Controller/LoginController.swift
+++ gameofchats/gameofchats/Controller/LoginController.swift
@@ -19,12 +19,25 @@ class LoginController: UIViewController {
         return view
     }()
     
+    let loginRegisterButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = UIColor(r: 80, g: 101, b: 161)
+        button.setTitle("Register", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor(r: 61, g: 91, b: 151)
         view.addSubview(inputsContainerView)
+        view.addSubview(loginRegisterButton)
+        
         setupInputsContainerView()
+        setupLoginRegisterButton()
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
@@ -37,6 +50,14 @@ class LoginController: UIViewController {
         inputsContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         inputsContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
         inputsContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+    }
+    
+    func setupLoginRegisterButton() {
+        // need x, y, width, height constraints
+        loginRegisterButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        loginRegisterButton.topAnchor.constraint(equalTo: inputsContainerView.bottomAnchor, constant: 20).isActive = true
+        loginRegisterButton.widthAnchor.constraint(equalTo: inputsContainerView.widthAnchor).isActive = true
+        loginRegisterButton.heightAnchor.constraint(equalToConstant: 30)
     }
 }
 
