1fea6eeab06f131cbe01b2bd7b701d0edb084f23
David Jung <david.jung@peeper.co>
2017-12-29 14:32:37 +0900

    [gameofchats] extension 을 이용해 RGB 값을 쉽게 선언할 수 있도록 convenience init 추가

--- gameofchats/gameofchats/Controller/LoginController.swift
+++ gameofchats/gameofchats/Controller/LoginController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class LoginController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor(red: 61/255, green: 91/255, blue: 151/255, alpha: 1)
+        view.backgroundColor = UIColor(r: 61, g: 91, b: 151)
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
+
+extension UIColor {
+    convenience init(r: CGFloat, g:CGFloat, b:CGFloat) {
+        self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
     }
 }
