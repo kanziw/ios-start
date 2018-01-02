a715a25a03f23b198c53dd700c2310c693834bc3
David Jung <david.jung@peeper.co>
2018-01-02 12:35:30 +0900

    [gameofchats] 메시지의 높이를 구해 동적으로 높이를 셋팅

--- gameofchats/gameofchats/View/ChatMessageCell.swift
+++ gameofchats/gameofchats/View/ChatMessageCell.swift
@@ -14,19 +14,35 @@ class ChatMessageCell: UICollectionViewCell {
         let tv = UITextView()
         tv.font = UIFont.systemFont(ofSize: 16)
         tv.translatesAutoresizingMaskIntoConstraints = false
+        tv.backgroundColor = .clear
+        tv.textColor = .white
         return tv
+    }()
+    
+    let bubbleView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(r: 0, g: 137, b: 249)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        addSubview(bubbleView)
         addSubview(textView)
+        
+        // x,y,w,h
+        bubbleView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        bubbleView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        bubbleView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        bubbleView.heightAnchor.constraint(equalTo: self.heightAnchor).isActive = true
         
         // ios 9 constraints
         // x,y,w,h
-        textView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        textView.leftAnchor.constraint(equalTo: bubbleView.leftAnchor, constant: 8).isActive = true
         textView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-        textView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        textView.rightAnchor.constraint(equalTo: bubbleView.rightAnchor).isActive = true
         textView.heightAnchor.constraint(equalTo: self.heightAnchor).isActive = true
     }
     
